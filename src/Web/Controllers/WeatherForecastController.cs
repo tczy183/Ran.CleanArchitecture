797855a3f2d74d08bd41daa94747ab291f78c20c
yang <tczy183@gmail.com>
2024-07-19 16:59:10 +0800

    Revert "change controller"
    This reverts commit daa5438b3147befa54368a543946fc5b52a24059.

--- src/Web/Controllers/WeatherForecastController.cs
+++ src/Web/Controllers/WeatherForecastController.cs
@@ -8,14 +8,20 @@ namespace Web.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediator)
-    : ControllerBase
+public class WeatherForecastController : ControllerBase
 {
-    private readonly ILogger<WeatherForecastController> _logger = logger;
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IMediator _mediator;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger,IMediator mediator)
+    {
+        _logger = logger;
+        _mediator = mediator;
+    }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public async Task<IEnumerable<WeatherForecast>> Get()
     {
-        return await mediator.Send(new GetWeatherForecastsQuery());
+        return await _mediator.Send(new GetWeatherForecastsQuery());
     }
 }
